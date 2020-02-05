17e32a76a21f20db6e5fec3be0d57e0167886e47
packetspy <packetspy@gmail.com>
2020-02-05 15:42:48 -0300

    splash screen

---
+++ lib/src/pages/splashscreen/splash_screen_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:splash_screen_flutter/src/pages/splashscreen/splash_screen_widget.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: SplashScreenWidget());
+  }
+}
---
+++ lib/src/pages/splashscreen/splash_screen_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+import 'package:splash_screen_flutter/main.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class SplashScreenWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //return Container();
+    return Stack(
+      children: <Widget>[
+        SplashScreen(
+          seconds: 3,
+          backgroundColor: Colors.white,
+          navigateAfterSeconds: MyHomePage(title: "Flutter Demo"),
+          loaderColor: Colors.transparent,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Container(
+                height: 250,
+                width: 250,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/logo_mgb_preto.png"),
+                        fit: BoxFit.contain)),
+              ),
+            ),
+            Center(
+              child: Container(
+                height: 150,
+                width: 150,
+                child: FlareActor(
+                  "assets/animations/loading_water.flr",
+                  animation: "start",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
