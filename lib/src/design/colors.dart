aab101b8d9e8f95da80d7e12101f6bd7ed7d8595
packetspy <packetspy@gmail.com>
2020-02-06 11:13:01 -0300

    color system

---
+++ lib/src/design/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import './hexcolor.dart';
+
+class SystemColors {
+  static const Color colorBase = const Color.fromRGBO(8, 155, 218, 1.0);
+  static Color bg1 = HexColor("#3C8CE7");
+  static Color bg2 = HexColor("#00EAFF");
+}
